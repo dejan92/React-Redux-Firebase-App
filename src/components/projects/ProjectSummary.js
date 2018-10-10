9252ac0e94faee2c0215eaa5c6ed0d24250c9430
djovanov92 <djovanov92@gmail.com>
2018-10-10 09:59:09 +0200

    using example with dummy data

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-const ProjectSummary = () => {
+const ProjectSummary = ({project}) => {
   return (
     <div className="card z-depth-0 project-sumary">
       <div className="card-content grey-text text-darken-3">
-        <span className="card-title">Project Title</span>
+        <span className="card-title">{project.title}</span>
         <p>Posted By Dejan Jovanov</p>
         <p className="grey-text">9th Oktober, 2pm</p>
       </div>
